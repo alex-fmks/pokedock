3055dd3d52c1a8d156fcc5823fc26f1a779a50d6
Alexander <90181605+alex-fmks@users.noreply.github.com>
2025-10-23 15:24:46 +0200

    add request for type api endpoint

--- public/get-pokeapi.php
+++ public/get-pokeapi.php
@@ -24,16 +24,26 @@ $responseArray = json_decode($response, true);
 
 $count = count($responseArray['results']);
 
-for ($i = 0; $i < $count; $i++) {
-    $name = $responseArray['results'][$i]['name'];
-}
-
 $con = dbcon();
 
+$statement = $con->prepare("INSERT INTO pokemons(name, type) VALUES(:name, :type)");
 for($i = 0; $i < $count; $i++){
-    $statement = $con->prepare("INSERT INTO pokemons(name) VALUES(:name)");
     $name = $responseArray['results'][$i]['name'];
     $statement->bindParam(':name', $name);
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://pokeapi.co/api/v2/pokemon/' . ($i + 1),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+    $response = curl_exec($curl);
+    $responseType = json_decode($response, true);
+    $type = $responseType['types'][0]['type']['name'];
+    $statement->bindParam(':type', $type);
     $statement->execute();
 }
 
